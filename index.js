56a08c6a9e0883a49fa8cdd48c03b3b54831f9ec
AquilesDantas <aquilesdant@gmail.com>
2022-03-23 23:58:57 -0300

    Deploy 8

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express= require('express');
 require('dotenv').config();
 
 const app = express();
-// const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Execuntando na porta: ${PORT} </h1>`));
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.get('/', (req, res) => res.send(`<h1 Execuntando na porta: ${PORT} </h1>`));
+app.listen(PORT, () => console.log(`Online na porta: ${PORT}`));
