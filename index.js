920ebb655cb06b2772730f4860cc385349236ed9
AquilesDantas <aquilesdant@gmail.com>
2022-03-23 23:47:19 -0300

    Deploy 5

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express= require('express');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(`<h1> Execuntando na porta: ${PORT} </h1>`));
-app.listen(PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
